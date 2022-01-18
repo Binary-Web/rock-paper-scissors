085d489d0eee5d3e99d7b5f9997f4a4a7bb74a3a
ben <ben.carlo.cornico@gmail.com>
2022-01-17 23:35:34 -0800

    finish

--- index.js
+++ index.js
@@ -27,31 +27,70 @@ function playRound(playerSelection, computerSelection) {
     }    
 }
 
+
+
+
 function Jugde(playerSelection, computerSelection, result) {
     const computer = computerSelection.toUpperCase();
     const player = playerSelection.toUpperCase();
     if(result == "win") {
-        return `
-        USER: ${player}     COMPUTER: ${computer}
-        YOU WIN - ${player} beats ${computer}
+        document.querySelector(".result").innerHTML = `
+        <div>USER: ${player}&emsp;&emsp;     COMPUTER: ${computer}</div>
+        <div>YOU WIN - ${player} beats ${computer}</div>
         `
+        scoreBoardUpdate("user")
     } else if (result == "lose") {
-        return `
-        USER: ${player}     COMPUTER: ${computer}
-        YOU LOSE - ${computer} beats ${player}
+        document.querySelector(".result").innerHTML =  `
+        <div>USER: ${player}&emsp;&emsp;    COMPUTER: ${computer}</div>
+        <div>YOU LOSE - ${computer} beats ${player}</div>
         `
+        scoreBoardUpdate("computer")
     } else if (result == "tie") {
-        return `
-        USER: ${player}     COMPUTER: ${computer}
-        IT'S A TIE!
+        document.querySelector(".result").innerHTML =  `
+        <div>USER: ${player} &emsp;&emsp;     COMPUTER: ${computer}</div>
+        <div>IT'S A TIE!</div>
         `
     }
+    
 }
-  
-  const playerSelection = "SCISSORS";
-  const computerSelection = "scissors"
-  console.log(playRound(playerSelection, computerSelection));
+
+let user = 0;
+let computer = 0;
+function scoreBoardUpdate(winner) {
+    if(winner === "user") {
+        user += 1;
+        document.querySelector("#user").innerHTML = `USER: ${user}`
+    } else if (winner === "computer") {
+        computer += 1;
+        document.querySelector("#computer").innerHTML = `COMPUTER: ${computer}`
+    }
+    if(!(user <= 4 && computer <= 4)) {
+        alert(document.querySelector(".winner").innerHTML = `WINNER IS ${winner.toUpperCase()}`);
+        user = 0;
+        computer = 0;
+        document.querySelector("#user").innerHTML = `USER: ${user}`
+        document.querySelector("#user").innerHTML = `COMPUTER: ${computer}`
+    }
 
 
+
+
+
+}
+
+
+const rps = document.querySelectorAll('.options');
+const computerChoices = ["rock", "paper", "scissors"];
+
+function test() {
+    console.log(option)
+}
+
+rps.forEach(option => {
+    console.log(option)
+    option.addEventListener("click", () => {
+        playRound(option.innerHTML, computerChoices[Math.floor(Math.random() * 3)])
+    })
+})
 
 // PAPER > ROCK > SCISSOR > PAPER
